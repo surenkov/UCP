a0bf0465124b7c7f1fddbcecaed276fea5672e58
Savva Surenkov <savva.surenkov@outlook.com>
2015-10-25 13:18:02 +1000

    Initial commit

---
+++ Tests/LexerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Lexer;
+
+namespace Tests
+{
+    [TestClass]
+    public class LexerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tests/StateMachineTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StateMachine;
+
+namespace Tests
+{
+    [TestClass]
+    public class StateMachineTests
+    {
+        [TestMethod]
+        public void StateTests()
+        {
+            var s = new State();
+
+        }
+    }
+}
