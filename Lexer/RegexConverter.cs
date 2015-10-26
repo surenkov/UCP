91e06087d230413e72ed0dee7fd8b5b7f6ea0df1
Savva Surenkov <savva.surenkov@outlook.com>
2015-10-26 18:03:44 +1000

    Added infix-to-postfix regex converter and it's tests

---
+++ Lexer/RegexConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexer
+{
+    public class RegexConverter
+    {
+        private static readonly Dictionary<char, int> _precedence;
+
+        static RegexConverter()
+        {
+            _precedence = new Dictionary<char, int>
+            {
+                { '(', 1 }, { '|', 2 }, { '.', 3 }, { '?', 4 }, { '*', 4 }, { '+', 4 }
+            };
+        }
+
+
+        public string Regex { get; set; }
+
+        private static int Precedence(char c)
+        {
+            if (_precedence.ContainsKey(c))
+                return _precedence[c];
+            return 6;
+        }
+
+        public string Postfix()
+        {
+            var stack = new Stack<char>(Regex.Length);
+            var postfix = new StringBuilder(Regex.Length);
+
+            foreach (char c in Regex)
+            {
+                switch (c)
+                {
+                    case '(':
+                        stack.Push(c);
+                        break;
+                    case ')':
+                        while (stack.Peek() != '(')
+                            postfix.Append(stack.Pop());
+                        stack.Pop();
+                        break;
+                    default:
+                        while (stack.Count > 0 && Precedence(stack.Peek()) >= Precedence(c))
+                            postfix.Append(stack.Pop());
+                        stack.Push(c);
+                        break;
+                }
+            }
+
+            while (stack.Count > 0)
+                postfix.Append(stack.Pop());
+
+            return postfix.ToString();
+        }
+    }
+}
--- Tests/LexerTests.cs
+++ Tests/LexerTests.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Lexer;
+using Lexer;
 
 namespace Tests
 {
@@ -8,8 +8,21 @@ namespace Tests
     public class LexerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RegexConverterTests()
         {
+            var testData = new Dictionary<string, string>
+            {
+                { "a.b.c", "ab.c." },
+                { "a.b|c", "ab.c|" },
+                { "a.b+.c", "ab+.c." },
+                { "a.(b.b)+.c", "abb.+.c." }
+            };
+            var conv = new RegexConverter();
+            foreach (var pair in testData)
+            {
+                conv.Regex = pair.Key;
+                Assert.AreEqual(pair.Value, conv.Postfix());
+            }
         }
     }
 }
