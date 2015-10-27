f45f82a31377485f6913e1c35a9b9305d921e259
Savva Surenkov <savva.surenkov@outlook.com>
2015-10-27 22:12:36 +1000

    Changed testing framework from Visual Studio tests to NUnit

--- Tests/LexerTests.cs
+++ Tests/LexerTests.cs
@@ -1,28 +1,28 @@
-﻿using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 using Lexer;
 
 namespace Tests
 {
-    [TestClass]
-    public class LexerTests
-    {
-        [TestMethod]
-        public void RegexConverterTests()
-        {
-            var testData = new Dictionary<string, string>
-            {
-                { "a.b.c", "ab.c." },
-                { "a.b|c", "ab.c|" },
-                { "a.b+.c", "ab+.c." },
-                { "a.(b.b)+.c", "abb.+.c." }
-            };
-            var conv = new RegexConverter();
-            foreach (var pair in testData)
-            {
-                conv.Regex = pair.Key;
-                Assert.AreEqual(pair.Value, conv.Postfix);
-            }
-        }
-    }
+	[TestFixture]
+	public class LexerTests
+	{
+		[Test]
+		public void RegexConverterTests()
+		{
+			var testData = new Dictionary<string, string> {
+				{ "a.b.c", "ab.c." },
+				{ "a.b|c", "ab.c|" },
+				{ "a.b+.c", "ab+.c." },
+				{ "a.(b.b)+.c", "abb.+.c." }
+			};
+			var conv = new RegexConverter();
+			foreach (var pair in testData) {
+				conv.Regex = pair.Key;
+				Assert.AreEqual(pair.Value, conv.Postfix);
+			}
+		}
+	}
 }
+
--- Tests/StateMachineTests.cs
+++ Tests/StateMachineTests.cs
@@ -1,36 +1,37 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using StateMachine;
 
 namespace Tests
 {
-    [TestClass]
-    public class StateMachineTests
-    {
-        [TestMethod]
-        public void StateTests()
-        {
-            var s = new State();
-            Assert.IsFalse(s.Start);
-            Assert.IsFalse(s.Final);
+	[TestFixture]
+	public class StateMachineTests
+	{
+		[Test]
+		public void StateTests()
+		{
+			var s = new State();
+			Assert.IsFalse(s.Start);
+			Assert.IsFalse(s.Final);
 
-            s.Name = "TK_STATE";
-            Assert.AreEqual(s.Name, "TK_STATE");
+			s.Name = "TK_STATE";
+			Assert.AreEqual(s.Name, "TK_STATE");
 
-            s.Start = true;
-            Assert.IsTrue(s.Start);
-            Assert.IsFalse(s.Final);
+			s.Start = true;
+			Assert.IsTrue(s.Start);
+			Assert.IsFalse(s.Final);
 
-            s.Final = true;
-            Assert.IsTrue(s.Final);
-            Assert.IsTrue(s.Start);
+			s.Final = true;
+			Assert.IsTrue(s.Final);
+			Assert.IsTrue(s.Start);
 
-            s.Start = false;
-            Assert.IsFalse(s.Start);
-            Assert.IsTrue(s.Final);
+			s.Start = false;
+			Assert.IsFalse(s.Start);
+			Assert.IsTrue(s.Final);
 
-            var s2 = new State(s);
-            Assert.AreEqual(s, s2);
-        }
-    }
+			var s2 = new State(s);
+			Assert.AreEqual(s, s2);
+		}
+	}
 }
+
