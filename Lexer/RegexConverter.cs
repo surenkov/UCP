679ba4e1dd58c2995c26b109c12e8d9fd5be4d45
Savva Surenkov <savva.surenkov@outlook.com>
2015-11-03 00:28:04 +1000

    Add characters range in regex parser

--- Lexer/RegexConverter.cs
+++ Lexer/RegexConverter.cs
@@ -1,8 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace LexicalAnalyzer
 {
+    public class InvalidRegexException : Exception
+    {
+        public InvalidRegexException()
+        {
+        }
+
+        public InvalidRegexException(string message) : base(message)
+        {
+        }
+
+        public InvalidRegexException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
     public static class RegexConverter
     {
         private static readonly Dictionary<char, int> Precedence;
@@ -26,6 +42,38 @@ namespace LexicalAnalyzer
         public static string Postfix(string regex)
         {
             var tmp = new StringBuilder(regex.Length * 2);
+            int rpos = 0;
+            for (int lpos = 0; (lpos = regex.IndexOf('[', lpos)) != -1; lpos = rpos + 1)
+            {
+                int rtmp = rpos == 0 ? 0 : rpos + 1;
+                tmp.Append(regex.Substring(rtmp, lpos - rtmp));
+                rpos = regex.IndexOf(']', lpos);
+                if (rpos == -1)
+                    throw new InvalidRegexException("Bad brackets sequence");
+                int range = lpos + 1;
+                tmp.Append('(');
+                for (int next; (next = regex.IndexOf('-', range, rpos - range)) != -1; range = next + 1)
+                {
+                    if (next != range + 1)
+                        tmp.Append('|');
+                    char from = regex[next - 1];
+                    char to = regex[next + 1];
+                    if (from > to)
+                        throw new InvalidRegexException("Invalid characters range");
+                    tmp.Append(from);
+                    while (from < to)
+                    {
+                        tmp.Append('|');
+                        tmp.Append(++from);
+                    }
+                }
+                for (int i = range + 1; i < rpos; i++)
+                    tmp.Append(regex[i]);
+                tmp.Append(')');
+            }
+            regex = tmp.Append(regex.Substring(rpos == 0 ? 0 : rpos + 1)).ToString();
+            tmp.Clear();
+
             for (int i = 1; i < regex.Length; i++)
             {
                 tmp.Append(regex[i - 1]);
